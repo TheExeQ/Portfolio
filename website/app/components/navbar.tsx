bd06784b39452db1e555ae2e555bf91c2a2e2f1c
Samuel Ryberg <samuel.ryberg@gmail.com>
2023-03-12 21:35:19 +0100

    improvements to navbar

--- website/app/components/navbar.tsx
+++ website/app/components/navbar.tsx
@@ -1,8 +1,16 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 import { FaLinkedinIn, FaGithub, FaInstagram } from 'react-icons/fa';
 
 export default function Navbar(): JSX.Element {
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    }
+
     return (
         <div className="fixed w-full h-20 shadow-xl z-10">
             <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
@@ -24,17 +32,19 @@ export default function Navbar(): JSX.Element {
                             <li className="ml-10 text-sm hover:border-b">Contact</li>
                         </Link>
                     </ul>
-                    <div className="md:hidden">
-                        <AiOutlineMenu size={25}/>
+                    <div onClick={toggleMenu} className="md:hidden cursor-pointer">
+                        <AiOutlineMenu size={25} />
                     </div>
                 </div>
             </div>
 
-            <div className="md:hidden fixed left-0 top-0 w-full h-screen bg-black/70">
-                <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] bg-[#ecf0f3] p-10 ease-in duration-500">
+            <div className={showMenu ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : ""}>
+                <div className={showMenu
+                    ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
+                    : "fixed left-[-100%] top-0 p-10 ease-in duration-500"}>
                     <div className="flex w-full items-center justify-between">
                         <h1 className="text-3xl font-bold">Samuel</h1>
-                        <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
+                        <div onClick={toggleMenu} className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
                             <AiOutlineClose />
                         </div>
                     </div>
@@ -59,20 +69,26 @@ export default function Navbar(): JSX.Element {
                         <div className="pt-40">
                             <p className="uppercase tracking-widest text-[#5651e5]">Let's Connect</p>
                             <div className="flex items-center justify-between my-4 w-full sm:w-[75%]">
-                                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-110 ease-in duration-300">
-                                    <FaLinkedinIn />
-                                </div>
-                                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-110 ease-in duration-300">
-                                    <FaGithub />
-                                </div>
-                                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-110 ease-in duration-300">
-                                    <FaInstagram />
-                                </div>
+                                <a href="https://www.linkedin.com/in/samuelryberg/" target="_blank">
+                                    <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-110 ease-in duration-300">
+                                        <FaLinkedinIn />
+                                    </div>
+                                </a>
+                                <a href="https://github.com/theexeq/" target="_blank">
+                                    <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-110 ease-in duration-300">
+                                        <FaGithub />
+                                    </div>
+                                </a>
+                                <a href="https://www.instagram.com/samuelryberg/" target="_blank">
+                                    <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-110 ease-in duration-300">
+                                        <FaInstagram />
+                                    </div>
+                                </a>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
+            </div >
+        </div >
     )
 }
