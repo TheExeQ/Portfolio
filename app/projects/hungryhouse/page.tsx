728b55a6e08ea4ec8b1b386716beef1e17dc9bfa
Samuel Ryberg <samuel.ryberg@gmail.com>
2023-04-02 23:30:26 +0200

    added awards to hungry house desc

--- app/projects/hungryhouse/page.tsx
+++ app/projects/hungryhouse/page.tsx
@@ -8,7 +8,7 @@ const projectProp: ProjectProps = {
     platform: "Mobile",
     productionTime: "6 weeks (50%)",
     teamSize: "13",
-    contributions: ["Main Menu", "Progress Saving/Loading", "Creature Animations"],
+    contributions: ["Main Menu", "Progress Saving/Loading", "Animation Implementations"],
 
     description:
         (
@@ -17,7 +17,12 @@ const projectProp: ProjectProps = {
                 The game's objective was to move a group of frogs between platforms, in order to feed a hungry living house creature. <br /><br />
                 What made the game challenging was that the creature couldn't be overfed or underfed, so players had to think strategically about how to move the frogs in order to get the right amount of food to the creature. <br /><br />
                 As the game progressed, it became even more challenging as obstacles were introduced.
-                These obstacles required players to carefully consider the movements of the frogs, in order to successfully navigate the level and keep the creature happy.
+                These obstacles required players to carefully consider the movements of the frogs, in order to successfully navigate the level and keep the creature happy. <br /><br />
+                <p className="font-bold text-2xl pb-2">Nominations & Awards</p>
+                <ul className="list-inside list-disc">
+                    <li><a className="text-blue-400 underline" href="https://www.gameawards.se/2022-nominees">Mobile Game of the Year and Best Art at the Swedish Game Awards 2022</a></li>
+                    <li><a className="text-blue-400 underline" href="https://www.therookies.co/entries/16448">Mobile Game of the Year - People's Choice at The Rookies Awards 2022</a></li>
+                </ul>
             </div>
         ),
     trailer: "/assets/projects/hungryhouse/hungryhouse.mp4",
