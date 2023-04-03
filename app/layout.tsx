975fddc73d9200bbdc734fcc17d4e7b92861b4e6
Samuel Ryberg <samuel.ryberg@gmail.com>
2023-04-03 17:39:29 +0200

    added resume to website

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }):
       <body>
         <Navbar />
         {children}
-        {/*<Footer />*/}
       </body>
     </html>
   )
